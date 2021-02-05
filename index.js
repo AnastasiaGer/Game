9854a76230ac2b3ba55d43b7bb0e34536332ef92
AnastasiaGer <aagermanova@gmail.com>
2021-02-05 15:42:49 -0800

    random box

--- index.js
+++ index.js
@@ -23,17 +23,26 @@ function handelBoxClick(event) {
 function renderBox() {
   game.innerHTML = ''
 let box = document.createElement('div')
+let boxSize = getRandom(30, 100)
+let gameSize = game.getBoundingClientRect()
+let maxTop = gameSize.height - boxSize
+let maxLeft = gameSize.width - boxSize
 
 box.style.backgroundColor = "black"
 box.style.position = "absolute"
 
-box.style.width = "50px"
-box.style.height = "50px"
+box.style.width = boxSize + 'px'
+box.style.height = boxSize + 'px'
 
-box.style.top = "50px"
-box.style.left = "70px"
+box.style.top = getRandom(0, maxTop) + 'px'
+box.style.left = getRandom(0, maxLeft) + 'px'
 box.style.cursor = "pointer"
 box.setAttribute('data-box', true)
 
 game.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom (min, max) {
+return Math.floor(Math.random() * (max-min) + min)
+
 }
