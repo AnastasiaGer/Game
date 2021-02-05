52c01006b23e3e51511c58882ff178cf4ada51d9
AnastasiaGer <aagermanova@gmail.com>
2021-02-05 15:35:22 -0800

    box

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const btn = document.querySelector('.btn')
 const game = document.querySelector('.game')
 
+let score = 0 
+
 btn.addEventListener('click', start)
+game.addEventListener('click', handelBoxClick)
 
 function start() {
   btn.classList.add('hide')
@@ -10,6 +13,27 @@ function start() {
   renderBox()
 }
 
-function renderBox() {
+function handelBoxClick(event) {
+  if(event.target.dataset.box) {
+    score++
+    renderBox()
+  }
+}
 
+function renderBox() {
+  game.innerHTML = ''
+let box = document.createElement('div')
+
+box.style.backgroundColor = "black"
+box.style.position = "absolute"
+
+box.style.width = "50px"
+box.style.height = "50px"
+
+box.style.top = "50px"
+box.style.left = "70px"
+box.style.cursor = "pointer"
+box.setAttribute('data-box', true)
+
+game.insertAdjacentElement('afterbegin', box)
 }
