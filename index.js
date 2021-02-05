9d9003054145d63c6dd11967d5b8271720232b73
AnastasiaGer <aagermanova@gmail.com>
2021-02-05 15:50:58 -0800

    timer

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const btn = document.querySelector('.btn')
 const game = document.querySelector('.game')
+const $time = document.querySelector('#time')
 
-let score = 0 
+let score = 0
+let isGameStarted = false
+
 
 btn.addEventListener('click', start)
 game.addEventListener('click', handelBoxClick)
 
 function start() {
+  isGameStarted = true
   btn.classList.add('hide')
   game.style.backgroundColor = "white"
+
+  let interval = setInterval(function () {
+    let time = parseFloat($time.textContent)
+    if (time <= 0) {
+      clearInterval(interval)
+      endGame()
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1)
+    }
+  }, 100)
 
   renderBox()
 }
 
+function endGame() {
+  isGameStarted = false
+}
+
 function handelBoxClick(event) {
-  if(event.target.dataset.box) {
+  if (!isGameStarted) {
+    return
+  }
+
+  if (event.target.dataset.box) {
     score++
     renderBox()
   }
@@ -22,27 +44,27 @@ function handelBoxClick(event) {
 
 function renderBox() {
   game.innerHTML = ''
-let box = document.createElement('div')
-let boxSize = getRandom(30, 100)
-let gameSize = game.getBoundingClientRect()
-let maxTop = gameSize.height - boxSize
-let maxLeft = gameSize.width - boxSize
+  let box = document.createElement('div')
+  let boxSize = getRandom(30, 100)
+  let gameSize = game.getBoundingClientRect()
+  let maxTop = gameSize.height - boxSize
+  let maxLeft = gameSize.width - boxSize
 
-box.style.backgroundColor = "black"
-box.style.position = "absolute"
+  box.style.backgroundColor = "black"
+  box.style.position = "absolute"
 
-box.style.width = boxSize + 'px'
-box.style.height = boxSize + 'px'
+  box.style.width = boxSize + 'px'
+  box.style.height = boxSize + 'px'
 
-box.style.top = getRandom(0, maxTop) + 'px'
-box.style.left = getRandom(0, maxLeft) + 'px'
-box.style.cursor = "pointer"
-box.setAttribute('data-box', true)
+  box.style.top = getRandom(0, maxTop) + 'px'
+  box.style.left = getRandom(0, maxLeft) + 'px'
+  box.style.cursor = "pointer"
+  box.setAttribute('data-box', true)
 
-game.insertAdjacentElement('afterbegin', box)
+  game.insertAdjacentElement('afterbegin', box)
 }
 
-function getRandom (min, max) {
-return Math.floor(Math.random() * (max-min) + min)
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
 
 }
